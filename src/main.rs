93ab10bb62f7f8560c1d1b39490e856ce6ea5f5b
Erik <erikdesjardins@users.noreply.github.com>
2018-04-14 23:57:25 -0400

    fmt

--- src/main.rs
+++ src/main.rs
@@ -24,12 +24,14 @@ mod parse;
 mod util;
 
 fn main() {
-    let ast = parse::parse(r#"
+    let ast = parse::parse(
+        r#"
         (function f(x) {
             while (true);
             return +[1, { x }, f + 1, ++g];
         });
-    "#).unwrap();
+    "#,
+    ).unwrap();
     let ir = ast2ir::convert(ast);
     println!("{:?}", ir);
 }
